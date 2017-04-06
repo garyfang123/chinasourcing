561cdb3afb6a11d9ea780fe196b4eb39fd0eac1b
Gary <gfang@vt.edu>
2017-04-05 23:05:08 -0400

    AppStrap Theme upload everything

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
 // app.set('view engine', 'ejs');
-
 app.use(express.static(__dirname + '/view'));
 
 // make express look in the public directory for assets (css/js/img)
@@ -19,6 +18,8 @@ app.get('/', function(req, res) {
 	// ejs render automatically looks in the views folder
 	res.render('index');
 });
+
+app.set('view engine', 'html');
 
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
